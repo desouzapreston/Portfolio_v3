4fd44444daaf2861073192ed6a1bdf645eaf76a3
Preston <desouzapreston@gmail.com>
2020-09-26 19:05:47 -0500

    Swapping to Firebase v2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCRY3YTN93V9cjzDlIyHYJev6kWNV2PK30",
+    authDomain: "portfolio-up.firebaseapp.com",
+    databaseURL: "https://portfolio-up.firebaseio.com",
+    projectId: "portfolio-up",
+    storageBucket: "portfolio-up.appspot.com",
+    messagingSenderId: "171248086829",
+    appId: "1:171248086829:web:1a1bc712ee0212f43b6da4",
+    measurementId: "G-9NYQ1XJT10"
+  },
+
 };
